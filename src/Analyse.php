93b5eba32cc4329c95bd2b6320bed66c56988afb
Antoine07 <antoine.lucsko@gmail.com>
2022-01-01 18:37:24 +0100

    first test Analyse message

--- src/Analyse.php
+++ src/Analyse.php
@@ -3,5 +3,21 @@
 namespace App\Analyse;
 
 class Analyse{
+
+    private $handle;
+    private $file;
     
+    public function __construct(string $file)
+    {
+        $this->file = $file;
+    }
+
+    public function loadData(string $file){
+        $this->handle = fopen($file, "r"); 
+    }
+
+    public function __toString()
+    {
+        return "Analyse Data Titanic";
+    }
 }
--- tests/AnalyseTest.php
+++ tests/AnalyseTest.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Analyse\Analyse;
 use PHPUnit\Framework\TestCase;
 
 class AnalyseTest extends TestCase
 {
+
+    private string $analyse;
+
+    public function setUp(): void
+    {
+        $this->analyse = new Analyse($file = __DIR__ . "/_Data/titanic.csv");
+    }
+
+    public function testMessage()
+    {
+        $this->assertEquals("Hello", (string) $this->analyse);
+    }
 }
